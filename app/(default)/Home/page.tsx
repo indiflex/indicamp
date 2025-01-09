0f02a578d2f86d36db9a971b47a254fc44f9071b
jykang24 <ttochi0105@naver.com>
2025-01-09 18:42:15 +0900

    feat: Add edge component

--- app/(default)/Home/page.tsx
+++ app/(default)/Home/page.tsx
@@ -1,12 +1,14 @@
 import Benefit from '@/components/benefit';
 import Challenge from '@/components/challenge';
+import Edge from '@/components/edge';
 import Faq from '@/components/faq';
 import Intro from '@/components/intro';
 
 export default function HomePage() {
   return (
     <>
-      <Intro />;
+      <Intro />
+      <Edge />
       <Challenge />;
       <Benefit />;
       <Faq />;
